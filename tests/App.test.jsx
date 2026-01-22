73187570c1e14e207b86ad3bd60c3b4c42307eb1
KarimKhaledMhammed <karimkhaledmohammed@gmail.com>
2026-01-22 13:37:11 +0200

    updated

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -6,7 +6,7 @@ import App from "../src/App";
 describe("App", () => {
   test("renders heading and hello component", () => {
     render(<App />);
-    expect(screen.getByText(/React Static Site/i)).toBeInTheDocument();
+    expect(screen.getByText(/Hello from my AWS Pipeline/i)).toBeInTheDocument();
     expect(screen.getByTestId("hello")).toHaveTextContent("Hello, railway!");
   });
 });
